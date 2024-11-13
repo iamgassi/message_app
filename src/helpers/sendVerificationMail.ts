cbc46418ad66ffc21594925e69c128ac00f9be24
btGauravGassi <118798729+btGauravGassi@users.noreply.github.com>
2024-11-14 01:02:33 +0530

    update email send API

--- src/helpers/sendVerificationMail.ts
+++ src/helpers/sendVerificationMail.ts
@@ -1,6 +1,5 @@
-import { resend } from "@/lib/resend";
-import EmailTemplate from "@/components/EmailTemplate";
 import { ApiResponse } from "@/types/apiResponse";
+import axios from "axios";
 
 export async function sendVerificationEmail(
     email: string,
@@ -8,19 +7,38 @@ export async function sendVerificationEmail(
     verifyCode: string
 ) : Promise<ApiResponse> {
     try {
-        const { data, error } = await resend.emails.send({
-            from: 'Acme <onboarding@resend.dev>',
-            to: [email],
-            subject: 'Secret Message | Verification code',
-            react: EmailTemplate({ username, verifyCode }),
-          });
+        // resend api
+        // const { data, error } = await resend.emails.send({
+        //     from: 'Acme <onboarding@resend.dev>',
+        //     to: [email],
+        //     subject: 'Secret Message | Verification code',
+        //     react: EmailTemplate({ username, verifyCode }),
+        // });
+        // if(error){
+        //     return {
+        //         success: false,
+        //         message: error?.message
+        //     } 
+        // }
 
-        if(error){
-            return {
-                success: false,
-                message: error?.message
-            } 
-        }
+        // Brevo api 
+        const apiKey = process.env.BREVO_API_KEY
+        const url = 'https://api.brevo.com/v3/smtp/email';
+      
+        const emailData = {
+          sender: { email: 'gauravgassi123@gmail.com', name: 'Secret Message' },
+          to: [{ email: email, name: username }],
+          subject: 'Verification Code',
+          htmlContent: `<div><h1>Hello , ${username}</h1><p>Your verification code is : ${verifyCode}</p></div>`,
+        };
+
+        await axios.post(url, emailData, {
+            headers: {
+              'api-key': apiKey,
+              'Content-Type': 'application/json',
+            },
+        });
+
         return {
             success: true,
             message: 'Verification email send successfully'
