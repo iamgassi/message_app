1efa5cbc9acb49ac6f3b0317087a8e08d4c46f12
btGauravGassi <118798729+btGauravGassi@users.noreply.github.com>
2024-11-23 20:37:44 +0530

    Added Gemini

--- src/app/u/[username]/page.tsx
+++ src/app/u/[username]/page.tsx
@@ -1,7 +1,7 @@
 'use client'
 import { Textarea } from '@/components/ui/textarea'
 import { useParams } from 'next/navigation'
-import React from 'react'
+import React, { useCallback, useState } from 'react'
 import { useForm } from 'react-hook-form'
 import { z } from 'zod'
 import { messageSchema } from '@/schemas/messageSchema'
@@ -11,12 +11,14 @@ import axios, { AxiosError } from 'axios'
 import { ApiResponse } from '@/types/apiResponse'
 import { Form, FormControl, FormField, FormItem, FormLabel, FormMessage } from "@/components/ui/form"
 import { Button } from "@/components/ui/button"
-import Navbar from '@/components/Navbar'
+import { Loader2 } from 'lucide-react'
+import { Separator } from "@/components/ui/separator"
 
 const Page = () =>{
     const params = useParams()
     const { toast } = useToast() 
-
+    const [messages, setMessages] = useState([])
+    const [isLoading, setIsLoading] = useState(false)
     const form = useForm<z.infer<typeof messageSchema>>({
         resolver: zodResolver(messageSchema)
     })
@@ -45,11 +47,37 @@ const Page = () =>{
             })
         }
     }
+
+    const suggestMessages = useCallback(async (refresh: boolean = false) => {
+    setIsLoading(true)
+    try {
+        const { data }:any = await axios.get<ApiResponse>('/api/suggest-messages')
+        let messages = data?.messages?.message?.split('||')?.map((item:any) => item?.trim());
+        setMessages(messages || [])
+        if(refresh){
+            toast({
+                title: 'Suggested Messages',
+                description: 'Suggested messages fetched',
+                variant: 'default'
+            })
+        }
+    } catch (error) {
+        const axiosError = error as AxiosError<ApiResponse>
+        toast({
+            title: 'Error',
+            description: axiosError.response?.data.message || 'Failed to refresh messages',
+            variant: 'destructive'
+        })
+    } finally {
+        setIsLoading(false)
+    }
+    },[setIsLoading, setMessages, toast])
+
     return (<>
-        <Navbar/>
-        <div className='container m-16'>
+        <div className='container my-6'>
+            <h2 className='text-2xl font-bold'>Public Profile URL</h2>
             <p className='my-4'>
-                Hi, <span className='capitalize font-bold'>{params?.username} </span> Here , Please write your Message for me secretly.
+               Please write your message secretly here.
             </p>
             <Form {...form}>
                     <form onSubmit={form.handleSubmit(onSubmit)} 
@@ -60,9 +88,8 @@ const Page = () =>{
                         name="content"
                         render={({ field }) => (
                         <FormItem>
-                        <FormLabel>Message for me</FormLabel>
+                        <FormLabel>Message for <span className='capitalize font-bold'>{params?.username} </span></FormLabel>
                         <FormControl>
-                            {/* <Input placeholder="code" {...field} /> */}
                             <Textarea {...field} placeholder="Type your message here." />
                         </FormControl>
                         <FormMessage />
@@ -71,8 +98,33 @@ const Page = () =>{
                     />
                     <Button type="submit"> Submit </Button>
                     </form>
-                </Form>
-
+            </Form>
+        </div>
+        <div className='py-2'>
+           <Separator />
+        </div>
+        <div className='container'>
+            <Button
+                className="mt-4 block"
+                onClick={(e) => {
+                    e.preventDefault()
+                    suggestMessages(true)
+                }}>
+                {isLoading? (
+                    <Loader2 className="h-4 w-4 animate-spin" />
+                ): (
+                    <>Suggest Message</>
+                )}
+            </Button>
+            <div className='p-5 mt-5 flex flex-col justify-between'>
+                { messages && 
+                    <>
+                        {messages?.map((message:any,idx:any)=>(
+                            <li key={idx}>{message}</li>
+                        ))}
+                    </>
+                }
+            </div>
         </div>
     </>)
 }
